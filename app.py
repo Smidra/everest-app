14eca9642de5a70cb862d0508b2563e8f47926a6
Smidra <rad.smid@gmail.com>
2021-06-27 18:05:39 +0200

    Index templated.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello!"
+def index():
+    return render_template('index.html')
 
 @app.route("/czech")
 def pozdrav():
