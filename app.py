b7419b6ff19f3b539c0f2612f9db5659047b4089
Smidra <rad.smid@gmail.com>
2021-06-28 17:15:07 +0200

    Progress bar with different colors.

--- app.py
+++ app.py
@@ -9,13 +9,22 @@ app.y = 0
 INCREMENT=1
 MAXIMUM=100
 
+def htmlify(cislo):
+    return "width: "+ str(cislo) + "%"
+
 @app.route("/")
 def index():
-    return render_template('index.html', r=app.r, b=app.b, g=app.g, y=app.y, maximum=MAXIMUM)
+    return render_template('index.html',
+    r=app.r, b=app.b, g=app.g, y=app.y,
+    maximum=MAXIMUM,
+    prog_r=htmlify(app.r),
+    prog_b=htmlify(app.b),
+    prog_g=htmlify(app.g),
+    prog_y=htmlify(app.y))
 
 @app.route("/klikadlo")
 def klikadlo():
-    return render_template('klikadlo.html', progress="width: "+ str(app.r+app.b+app.g+app.y) + "%", maximum=MAXIMUM)
+    return render_template('klikadlo.html', progress=htmlify(app.r+app.b+app.g+app.y), maximum=MAXIMUM)
 
 # Pridavani prichozich hodnot
 @app.route("/r")
